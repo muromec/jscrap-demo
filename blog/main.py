4839e1434b9fde2e2933c9532a4f9e23211a2e57
Ilya Petrov <ilya.muromec@gmail.com>
2011-11-30 23:14:07 +0200

    add comments to code

--- blog/main.py
+++ blog/main.py
@@ -5,7 +5,68 @@ from biribiri import chain
 from biribiri.chain.utils import upd_ctx, view, match
 from jinja2 import Environment, FileSystemLoader
 
+"""
+Ok, whats going on here?
+
+First, look at entry point, called "application"
+"""
+
+
+@Request.application
+def application(request):
+    """
+    Typical werkzeug app, nothing special.
+
+    Here I`m handling incoming request and passing it to
+    chain of functions (route, response).
+
+    Execution context is almost empty, except our request.
+
+    """
+
+    ret = chain.run([route, response], request=request)
+    return ret['response']
+
+@upd_ctx('url', 'mime')
+def route(request, **ctx):
+    """
+    This function called route.
+
+    Really it just grabs some data from request and puts
+    back into execution context as variables "url" 
+    and "mime".
+
+    This is the meaning of @upd_ctx - update context with
+    values, returned from function.
+
+    First one is relative url and mime is set of all
+    mime-types, browser claimed to understand.
+
+    Url used to route request into specific @view
+    and "mime" used to determine, how execution
+    context would be rendered back to user
+
+    And the most important part is last: handlers list.
+    Handlers list may look like url map, but it`s not one.
+
+    Url mapper selects just *one* function to handle url,
+    handlers list specifies *many* functions, calling
+    one by one. Really, it calls *every* function in
+    handlers list, but some are filtered out by decorators.
+    """
+
+    mime_set = set([
+        mt
+        for mt,prio in request.accept_mimetypes
+    ])
+    return request.path, mime_set, handlers
+
+
 def f(filename, f=__file__, sep=""):
+    """
+    Dont mind me, I`m just a helper function
+    to find, where my templates are
+    """
     path,_file = os.path.split(f)
     return os.path.join(path, sep, filename)
 
@@ -14,6 +75,16 @@ env = Environment( loader=FileSystemLoader(
 ))
 
 def accept(*know_mime):
+    """
+    Here is simple decorator, choosing which
+    render function to run.
+
+    Using request.args is a bit of hack, as browsers
+    are not smart enought to tell me, what they want
+
+    Look, its not *running* function, but returning
+    it same way, as "route" returns handlers list
+    """
     know_set = set(know_mime)
     def decorator(f):
         def wrapped(*a, **kw):
@@ -34,10 +105,23 @@ def accept(*know_mime):
 
 @upd_ctx('tpl', 'dump_fields')
 def splash(**ctx):
+    """
+    This function selects template used to render
+    page and list of context variables which are
+    safe to dump to user in json request
+
+    How this functon called? Look at handlers list.
+    
+    """
     return 'splash.html', [ 'nav', 'gen', 'post_list', 'url',],None
 
 @upd_ctx('nav')
 def load_links(**ctx):
+    """
+    I`m lazy. No real database here, just hardcoded result.
+
+    All this vars are availiable inside template.
+    """
     return {
         "links": [
             {"url": "/?js", "name": "Template source",},
@@ -58,6 +142,10 @@ def load_links(**ctx):
 
 @upd_ctx('post_list')
 def blog_posts(**ctx):
+    """
+    Same as above, adding data to context to catch it back
+    inside template.
+    """
     return [
             {
                 "title": "JSCrap", 
@@ -73,12 +161,24 @@ def blog_posts(**ctx):
 
 @upd_ctx('gen')
 def gen_by(**ctx):
+    """
+    Add text to footer
+    """
     import jinja2
     return "Jinja %s" % jinja2.__version__
 
 @accept('text/javascript', 'js')
 @upd_ctx('body', 'ct')
 def dump_tpl(tpl, **ctx):
+    """
+    Here I`m compiling template into js code
+    and dumping it to browser.
+
+    Template selected by function "splash" which is 
+    view function for "/".
+
+    This handler called every time, somebody GETs /?js
+    """
     from jinja2.parser import Parser
     from jscrap.generator import JsGenerator
 
@@ -94,6 +194,13 @@ def dump_tpl(tpl, **ctx):
 @match(tpl=basestring)
 @upd_ctx('body')
 def render_html(tpl, **ctx):
+    """
+    Wow, browser is smart enough to understand HTML!
+
+    Renderig whole context for such case.
+
+    Template name selected by "splash"
+    """
     tpl_o = env.get_template(tpl)
 
     return tpl_o.render(**ctx)
@@ -102,6 +209,12 @@ def render_html(tpl, **ctx):
 @match(dump_fields=list)
 @upd_ctx('body', 'ct')
 def render_json(dump_fields, **ctx):
+    """
+    Somebody called  /?json. Ok, dump it.
+
+    Its same context, as in every "/" call, but
+    result consists of safe vars, serilized to json.
+    """
     return dumps(dict([
         (field, ctx[field])
         for field in dump_fields
@@ -111,21 +224,23 @@ def render_json(dump_fields, **ctx):
 
 @upd_ctx('response')
 def response(body=None, ct='text/html', **ctx):
+    """
+    Nothing interesting. If somebody pushed "body" 
+    into context, return it. If nothing it`s 404.
+    """
     code = 202 if body else 404
     return Response(body or "Nani-Nani", status=code, content_type=ct)
-
-@upd_ctx('url', 'mime')
-def route(request, **ctx):
-    mime_set = set([
-        mt
-        for mt,prio in request.accept_mimetypes
-    ])
-    return request.path, mime_set, handlers
 
 @match(found_view=None, body=None)
 @upd_ctx('body')
 def static(url, **ctx):
+    """
+    I should call special people to configure nginx.
+
+    Dont serve static in this way!
+    """
     if not url.startswith('/static'):
+        # yeah, this is ugly!
         return
 
     _,static,package,path = url.split('/',3)
@@ -133,13 +248,23 @@ def static(url, **ctx):
         return
 
     mod = __import__(package)
-    return open(f(path, mod.__file__, 'data'), 'rb')
+    try:
+        return open(f(path, mod.__file__, 'data'), 'rb')
+    except IOError:
+        return
 
+"""
+Here is handlers list.
+
+Functins called from down to up.
+
+
+"""
 handlers = [
         static,
-        dump_tpl,
-        render_json,
-        render_html,
+        dump_tpl,    # one of this three should render
+        render_json, # body into something
+        render_html, # readable by browser.
         gen_by,
 
         view("/plain")([
@@ -148,16 +273,15 @@ handlers = [
             splash,
         ]),
         view('/')([
+            # Every function in this block called
+            # to render splash template.
+            #
+            # Again. From bottom to top.
             blog_posts,
             load_links,
             splash,
         ]),
 ]
-
-@Request.application
-def application(request):
-    ret = chain.run([route, response], request=request)
-    return ret['response']
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
